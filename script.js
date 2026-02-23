d98248991fd79b3a50da479b8915e590dd40b981
musfik070-gif <musfik070@gmail.com>
2026-02-23 19:17:17 +0600

    Js file, created 1 job list and current tab

---
+++ script.js
@@ -0,0 +1,16 @@
+//job list
+let jobs = [
+  { id:1,
+    company:"Mobile First Corp",
+    position:"React Native Developer",
+    location:"Remote", 
+    type:"Full-time",
+    salary:"$130,000 - $175,000", 
+    description:"Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.", 
+    status:"none"},
+
+];
+
+//Initial tab
+let currentTab = "all";
+
